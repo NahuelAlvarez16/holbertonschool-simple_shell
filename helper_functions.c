e8cf631b681deea4e8ef6a9e7963e144e58f4da9
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-28 10:38:50 -0700

    Create helper functions

---
+++ helper_functions.c
@@ -0,0 +1,86 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+/**
+ * _strcpy - Copies the string pointed to by src
+ * @dest: Destined
+ * @src: Contens
+ * Return: return src variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
+ * _strcmp - Comparate two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: Result of the comparatin
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+/**
+ * _strlen - lenght of a string
+ * @s: string
+ * Return: return lenght of a s variable
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*(s + lenght))
+		lenght++;
+	return (lenght);
+}
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Destined
+ * @src: Contens
+ * Return: return src variable
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int length = 0;
+
+	while (*(dest + length))
+		length++;
+
+	while (*(src + i))
+	{
+		*(dest + length) = *(src + i);
+		length++;
+		i++;
+	}
+	*(dest + length) = '\0';
+	return (dest);
+}
